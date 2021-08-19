3cec3d3d6444c6621e438e1330f2de1c135b189e
flchazelas <44674852+flchazelas@users.noreply.github.com>
2021-08-19 15:42:10 +0200

    Add files via upload

---
+++ js/contact_form.js
@@ -0,0 +1,71 @@
+/*$("#contactForm").validator().on("submit", function (event) {
+    if (event.isDefaultPrevented()) {
+        // handle the invalid form...
+    } else {
+        // everything looks good!
+        event.preventDefault();
+        submitForm();
+    }
+});
+
+function submitForm(){
+    var name = $("#name").val();
+    var email = $("#email").val();
+    var message = $("#message").val();
+ 
+    $.ajax({
+        type: "POST",
+        url: "php/form_process.php",
+        data: "name=" + name + "&email=" + email + "&message=" + message,
+        success : function(text){
+            if (text == "success"){
+                formSuccess();
+            }
+            else{
+                formError();
+                submitMSG(false,text);
+            }
+        }
+    });
+}
+function formSuccess(){
+    $("#contactForm")[0].reset();
+    submitMSG(true, "Message Submitted!")
+}
+
+function formError(){
+    $("#contactForm").removeClass().addClass('shake animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+        $(this).removeClass();
+    });
+}
+
+function submitMSG(valid, msg){
+    if(valid){
+        var msgClasses = "h3 text-center tada animated text-success";
+    } else {
+        var msgClasses = "h3 text-center text-danger";
+    }
+    $("#msgSubmit").removeClass().addClass(msgClasses).text(msg);
+}*/
+
+var form = document.getElementById("my-form");
+        
+        async function handleSubmit(event) {
+          event.preventDefault();
+          var status = document.getElementById("my-form-status");
+          var data = new FormData(event.target);
+          fetch(event.target.action, {
+            method: form.method,
+            body: data,
+            headers: {
+                'Accept': 'application/json'
+            }
+          }).then(response => {
+            status.innerHTML = "Message envoyé!";
+            form.reset()
+          }).catch(error => {
+            status.innerHTML = "Oops! There was a problem submitting your form"
+          });
+        }
+
+form.addEventListener("submit", handleSubmit);
---
+++ js/scroll_nav_bar.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+/*
+    $(".navbar").hide();
+
+    $(function(){
+        $(window).scroll(function(){
+            if($(this).scrollTop() > 100){
+                $('.navbar').fadeIn();
+            } 
+            else{
+                $('.navbar').fadeOut();
+            }
+        });
+    });*/
+});
